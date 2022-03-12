6a8d3c691a65fa85568267d7098fd3a31adbaa00
Maj <59331721+Majus1@users.noreply.github.com>
2022-03-12 14:58:06 +0100

    test

--- js/app.js
+++ js/app.js
@@ -2,23 +2,3 @@
 console.log("hello ninja turtles");
 
 // Setting up Owl caorusel
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  responsiveClass:true,
-  responsive:{
-      0:{
-          items:1,
-          nav:true
-      },
-      768:{
-          items:2,
-          nav:false
-      },
-      1000:{
-          items:5,
-          nav:true,
-          loop:false
-      }
-  }
-})
