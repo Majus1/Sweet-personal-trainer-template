103284cd760618cb1f36564ecef1b6081792e218
Maj <59331721+Majus1@users.noreply.github.com>
2022-03-12 15:28:21 +0100

    testing carousel

--- js/app.js
+++ js/app.js
@@ -2,3 +2,25 @@
 console.log("hello ninja turtles");
 
 // Setting up Owl caorusel
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
+});
