16beef1ccfbdb4f34e08a48eb06e1d53c9976b3d
Maj <59331721+Majus1@users.noreply.github.com>
2022-03-14 19:31:47 +0100

    Added some styling to the carousel

--- js/app.js
+++ js/app.js
@@ -2,25 +2,34 @@
 console.log("hello ninja turtles");
 
 // Setting up Owl caorusel
-$(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:5,
-            nav:true,
-            loop:false
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            768: {
+                items: 2,
+                nav: false
+            },
+            1200: {
+                items: 4,
+                nav: false,
+                loop: false
+            }
         }
-    }
-})
+    })
 });
+// Setting up Owl caorusel
+
+// Stylingy Owl carousel
+var testimonials = document.querySelectorAll(".item")
+
+for (let z = 0; z < testimonials.length; z++) {
+    testimonials[z].classList.add("py-4")
+    testimonials[z].classList.add("px-3")
+}
+// Stylingy Owl carousel
